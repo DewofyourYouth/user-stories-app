231b1c4bf79acc040017bbde9a253e63d01c8469
Jacob Shore <jacobshore@gmail.com>
2022-10-28 15:55:22 +0300

    remove options after choice responses.json

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,11 +1,13 @@
 import { makeUserInput } from "../../utils";
 // import HAL from "../assets/HAL.jpeg";
 import { useConversationStore } from "../store";
+import { useState } from "react";
 
-export function Chat({ system, message, options = [] }) {
+export function Chat({ system, message, options = [], action = null }) {
   const appendToConversation = useConversationStore(
     (state) => state.appendToConversation
   );
+  const [optionSelected, setOptionSelected] = useState(false);
   const divCls = system ? "chat systemPrompt" : "chat userResponse";
   const cls = system ? "systemHandle" : "userHandle";
   const userName = system ? "HAL" : "Dave";
@@ -17,17 +19,20 @@ export function Chat({ system, message, options = [] }) {
       {options.length > 0 && (
         <div className="buttonGroup">
           <ul>
-            {options.map((option) => (
-              <li key={option.key}>
-                <button
-                  onClick={(e) => {
-                    appendToConversation(makeUserInput(option.message));
-                  }}
-                >
-                  {option.message}
-                </button>
-              </li>
-            ))}
+            {optionSelected === false &&
+              options.map((option) => (
+                <li key={option.key}>
+                  <button
+                    onClick={(e) => {
+                      const userInput = makeUserInput(option.message);
+                      appendToConversation(userInput);
+                      setOptionSelected(true);
+                    }}
+                  >
+                    {option.message}
+                  </button>
+                </li>
+              ))}
           </ul>
         </div>
       )}
--- src/store.js
+++ src/store.js
@@ -4,9 +4,21 @@ const firstPrompt = {
   system: true,
   message: "Hello, Dave. What would you like to do today?",
   options: [
-    { key: 1, message: "I would like to make a new product." },
-    { key: 2, message: "I would like to review and / or modify a product." },
-    { key: 3, message: "I would like info about the process." },
+    {
+      key: 1,
+      message: "I would like to make a new product.",
+      action: "requestProductName",
+    },
+    {
+      key: 2,
+      message: "I would like to review and / or modify a product.",
+      action: "getProductList",
+    },
+    {
+      key: 3,
+      message: "I would like info about the process.",
+      action: "getInfoList",
+    },
   ],
 };
 
